882940712d89f11b7544adcf47731557afe3f3c4
kasra-kakavand <www.kasrakakavandup@gmail.com>
2026-05-17 21:55:26 +0330

    "Add version tracking module"

---
+++ faircheck/version.py
@@ -0,0 +1,12 @@
+"""Version information for FairCheck."""
+
+__version__ = "0.1.0"
+VERSION = __version__
+
+# Version components
+VERSION_MAJOR = 0
+VERSION_MINOR = 1
+VERSION_PATCH = 0
+
+# Release stage: alpha, beta, rc, or stable
+VERSION_STAGE = "alpha"
